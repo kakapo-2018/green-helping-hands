68ba84198259aec6a2208b21569702762d2c31e2
student <yuzuki.sadoh@gmail.com>
2018-08-16 19:20:17 +1200

    add addToGarden function - the function works

--- public/functions.js
+++ public/functions.js
@@ -1,9 +1,15 @@
 // when a vege button gets clicked push it in a new fs.write object
-let myGarden = require('./mygarden.json')
-let selectedVege = $('.vegetable')
+// let myGarden = require('./mygarden.json')
+let selectedVege = document.getElementsByClassName('vege')
 
-function addVegetoGardenObject(selectedVege){
-    selectedVege.addEventListener('click', function(){
-        myGarden.push()
-    })
+// document.getElementsByTagName("button").addEventListener("click", function(){
+//     alert('clicked');
+// })
+
+function addToGarden(){
+ alert('did stuff #1')
 }
+
+// .addEventListener("click", function(){
+//     document.getElementById("demo").innerHTML = "Hello World";
+// });
